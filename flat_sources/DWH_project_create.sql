885a216b43b7e9966b991213c427917faeb2ef57
solawak <kasia.solawa1@gmail.com>
2022-06-12 17:09:18 +0200

    update DWH_poject_create.sql

--- flat_sources/DWH_project_create.sql
+++ flat_sources/DWH_project_create.sql
@@ -39,6 +39,9 @@ CREATE TABLE [dbo].[OrdersFacts](
 	[add_to_cart_order] [int] NOT NULL,
 	[is_reordered] [nvarchar](40) NOT NULL,
 	[city_id] [int] NOT NULL,
+	[severity] [nvarchar](40) NOT NULL,
+	[weather_type] [nvarchar](40) NOT NULL,
+	[precipitation_inches] [float] NOT NULL
  CONSTRAINT [PK_OrdersFacts] PRIMARY KEY CLUSTERED 
 (
 	[order_product_id] ASC
@@ -53,6 +56,7 @@ CREATE TABLE [dbo].[Products](
 	[aisle] [nvarchar](40) NOT NULL,
 	[department] [nvarchar](40) NOT NULL,
 	[product_name] [nvarchar](40) NOT NULL,
+
  CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
 (
 	[product_id] ASC
@@ -75,45 +79,8 @@ CREATE TABLE [dbo].[Users](
 ) ON [PRIMARY]
 GO
 
-CREATE TABLE [dbo].[Weather](
-	[severity] [nvarchar](40) NOT NULL,
-	[start_time] [datetime] NOT NULL,
-	[end_date_id] [int] NOT NULL,
-	[event_id] [nvarchar](40) NOT NULL,
-	[city_name] [nvarchar](40) NOT NULL,
-	[weather_type] [nvarchar](40) NOT NULL,
-	[end_time] [datetime] NOT NULL,
-	[precipitation_inches] [float] NOT NULL,
-	[start_date_id] [int] NOT NULL,
-	[city_id] [int] NOT NULL,
- CONSTRAINT [PK_Weather_1] PRIMARY KEY CLUSTERED 
-(
-	[event_id] ASC,
-	[city_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
 
-ALTER TABLE [dbo].[Weather]  WITH CHECK ADD  CONSTRAINT [FK_Weather_Cities] FOREIGN KEY([city_id])
-REFERENCES [dbo].[Cities] ([city_id])
-GO
 
-ALTER TABLE [dbo].[Weather] CHECK CONSTRAINT [FK_Weather_Cities]
-GO
-
-ALTER TABLE [dbo].[Weather]  WITH CHECK ADD  CONSTRAINT [FK_Weather_DateDimension] FOREIGN KEY([start_date_id])
-REFERENCES [dbo].[DateDimension] ([DateID])
-GO
-
-ALTER TABLE [dbo].[Weather] CHECK CONSTRAINT [FK_Weather_DateDimension]
-GO
-
-ALTER TABLE [dbo].[Weather]  WITH CHECK ADD  CONSTRAINT [FK_Weather_DateDimension1] FOREIGN KEY([end_date_id])
-REFERENCES [dbo].[DateDimension] ([DateID])
-GO
-
-ALTER TABLE [dbo].[Weather] CHECK CONSTRAINT [FK_Weather_DateDimension1]
-GO
 
 
 
