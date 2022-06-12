b231e928922165486c8e44f76cf20302b3e75d17
MattS0000 <mati201420@gmail.com>
2022-06-12 13:32:49 +0200

    Update DWH_project_create.sql

--- flat_sources/DWH_project_create.sql
+++ flat_sources/DWH_project_create.sql
@@ -27,43 +27,6 @@ CREATE TABLE [dbo].[Cities](
 ) ON [PRIMARY]
 GO  
 
-CREATE TABLE [dbo].[DateDimension](
-	[DateID] [int] NOT NULL,
-	[Date] [date] NOT NULL,
-	[Day] [tinyint] NOT NULL,
-	[DaySuffix] [char](2) NOT NULL,
-	[Weekday] [tinyint] NOT NULL,
-	[WeekDayName] [varchar](10) NOT NULL,
-	[IsWeekend] [bit] NOT NULL,
-	[IsHoliday] [bit] NOT NULL,
-	[HolidayText] [varchar](64) SPARSE  NULL,
-	[DOWInMonth] [tinyint] NOT NULL,
-	[DayOfYear] [smallint] NOT NULL,
-	[WeekOfMonth] [tinyint] NOT NULL,
-	[WeekOfYear] [tinyint] NOT NULL,
-	[ISOWeekOfYear] [tinyint] NOT NULL,
-	[Month] [tinyint] NOT NULL,
-	[MonthName] [varchar](10) NOT NULL,
-	[Quarter] [tinyint] NOT NULL,
-	[QuarterName] [varchar](6) NOT NULL,
-	[Year] [int] NOT NULL,
-	[MMYYYY] [char](6) NOT NULL,
-	[MonthYear] [char](7) NOT NULL,
-	[FirstDayOfMonth] [date] NOT NULL,
-	[LastDayOfMonth] [date] NOT NULL,
-	[FirstDayOfQuarter] [date] NOT NULL,
-	[LastDayOfQuarter] [date] NOT NULL,
-	[FirstDayOfYear] [date] NOT NULL,
-	[LastDayOfYear] [date] NOT NULL,
-	[FirstDayOfNextMonth] [date] NOT NULL,
-	[FirstDayOfNextYear] [date] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[DateID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
 CREATE TABLE [dbo].[OrdersFacts](
 	[order_date_id] [int] NOT NULL,
 	[order_product_id] [int] NOT NULL,
